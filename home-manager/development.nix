1002340c992e1a3526a606858362eedc9fff9a99
elman <elman.teyubov@gmail.com>
2025-05-08 12:42:59 +0400

    home-manager: add c++ related utilities to development.nix

--- home-manager/development.nix
+++ home-manager/development.nix
@@ -9,7 +9,29 @@
   home.packages = with pkgs; [
     neovim
     vscode
+    #gcc
+    clang
+    clang-tools
+    cmake
+    ninja
+    python3
   ];
+
+  programs.vscode.profiles.default = {
+    enable = true;
+    enableUpdateCheck = true;
+    enableExtensionUpdateCheck = true;
+    extensions = with pkgs.vscode-extensions; [
+      ms-vscode.cpptools-extension-pack
+      vscodevim.vim
+      haskell.haskell
+    ];
+  };
+ 
+  home.sessionVariables = {
+    CC = "gcc";
+    CXX = "g++";
+  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
