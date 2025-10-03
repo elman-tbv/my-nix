edc2ea6fd0791a10f1c6c4cfc3daae5295da5824
Elman Teyubov <elman.teyubov@gmail.com>
2025-10-04 02:19:49 +0400

    add modules/hyperland.nix

---
+++ modules/hyprland.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, inputs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    waybar
+    mako # notification daemon
+    libnotify
+    hyprpaper # wallpapers
+    rofi  # app launcher
+    nautilus
+  ];
+
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    xwayland.enable = true;
+  };
+}
