ac9d5531b5ce13fcc6d5360424a194841eb1c833
Elman Teyubov <elman.teyubov@gmail.com>
2025-10-10 14:30:48 +0400

    linux-gui related module

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 {
   home.username = "elman";
   home.homeDirectory = "/home/elman";
   imports = [
     ../home-manager/default.nix
     ../home-manager/development.nix
+    ../home-manager/linux-gui.nix
   ];
 }
