9e5798596397cc0e304fda89b57764bb0a00764a
Elman Teyubov <elman.teyubov@gmail.com>
2025-10-27 00:30:40 +0400

    enable xwayland in hyprland.nix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -15,4 +15,6 @@
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     xwayland.enable = true;
   };
+
+  programs.xwayland.enable = true;
 }
