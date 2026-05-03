d8dfcc3ee74d59d1ce529e2870ca96557d5d7b7e
Elman Teyyubov <elman.teyubov@gmail.com>
2026-05-03 20:38:20 +0400

    dev tools module

---
+++ modules/development.nix
@@ -0,0 +1,49 @@
+{
+flake.homeModules.development = { self, config, pkgs, inputs, ... }:
+{
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg: builtins.elem (pkgs.lib.getName pkg) [
+      "vscode"
+    ];
+
+  home.packages = with pkgs; [
+    neovim
+    zed-editor
+    python3
+    nix-direnv
+    lazygit
+    qemu
+    nixd
+    docker
+    podman
+  ];
+
+  xdg.configFile."nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/nvim";
+    recursive = true;
+  };
+
+  xdg.configFile."kitty" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/kitty";
+    recursive = true;
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # ".screenrc".source = dotfiles/screenrc;
+  };
+};
+}
