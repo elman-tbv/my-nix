8982ca916e0470a49eb6aa4ade251af3ccda34d4
Elman Teyubov <elman.teyubov@gmail.com>
2026-06-04 02:07:42 +0400

    make home-manager config system agnostic

--- modules/home.nix
+++ modules/home.nix
@@ -1,15 +1,17 @@
 {self, inputs, ...}: {
-  flake.homeConfigurations = {
-    elman = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
-      modules = with self.homeModules; [
-        base
-        dotfiles
-        development
-        users
-        shell
-        apps
-      ];
+  perSystem = {pkgs, system, ...}: {
+    legacyPackages.homeConfigurations = {
+      elman = inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = with self.homeModules; [
+          base
+          dotfiles
+          development
+          users
+          shell
+          apps
+        ];
+      };
     };
   };
 }
